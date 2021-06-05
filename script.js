c87c0679611b7d4176b95ca13a821aedc8b8d36e
Bruno Richard <bruno683@outlook.fr>
2021-06-05 12:10:21 +0200

    Add prompt playerName features

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 class Player {
-  constructor(score, currentScore){
+  constructor(score, currentScore, name){
     this.score = score;
     this.currentScore = currentScore;
+    this.name = name;
+    this.isWinner = false;
+    this.round = false;
   }
 }
 
 const diceImage = document.querySelector('.diceImage');
-
+const playerNameOne = document.querySelector('#playerOne')
+const playerNameTwo = document.querySelector('#playerTwo')
 const playerOne = new Player();
 const playerTwo = new Player();
 playerOne.score = document.querySelector('#pOneScore');
@@ -15,14 +19,35 @@ playerOne.currentScore = document.querySelector('#pTwoScore')
 
 const roll = document.querySelector('.roll')
 
-
-
-
-function getRandomDice(min, max) {
+const getRandomDice = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1 )) + min;
 };
+
+function initGame () {
+
+  const pOneName = prompt('Joueur un: Quelle est votre prénom?');
+  const pTwoName = prompt('joueur deux: Quel est votre prénom')
+  
+  playerOne.name = pOneName;
+  playerTwo.name = pTwoName;
+  playerNameOne.innerHTML = `<p>${playerOne.name}</p>`;
+  playerNameTwo.innerHTML = `<p>${playerTwo.name}</p>`;
+  playerOne.round = true;
+  const value = () => {
+    return getRandomDice(1,6);
+  }
+  let number = value();
+    //console.log(number);
+  diceImage.innerHTML = `<img src="images/${number}_dots.png">`;
+
+
+  //init the game
+}
+
+initGame()
+
 
 roll.addEventListener('click',  () => {
  
@@ -31,6 +56,6 @@ roll.addEventListener('click',  () => {
   }
   let number = value()
     console.log(number);
-  diceImage.innerHTML = `<img src="images/${number}_dot.png">`;
+  diceImage.innerHTML = `<img src="images/${number}_dots.png">`;
 });
   
